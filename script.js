667ddfbfca8eb6ac8a40454fb96be794ef46ca98
JoelCruzCode <136981207+JoelCruzCode@users.noreply.github.com>
2023-07-16 06:45:53 -0700

    Create basic math functions and button variables

---
+++ script.js
@@ -0,0 +1,14 @@
+const subBtn = document.querySelector(".substract");
+const addBtn = document.querySelector(".add");
+const multBtn = document.querySelector(".multiply");
+const divBtn = document.querySelector(".divide");
+const equalBtn = document.querySelector(".equal");
+const clearBtn = document.querySelector(".clear");
+
+const add = (a, b) => a + b;
+
+const subtract = (a, b) => a - b;
+
+const multiply = (a, b) => a * b;
+
+const divide = (a, b) => a / b;
