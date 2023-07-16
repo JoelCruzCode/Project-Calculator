ae953310681922ef3a2d956c0a691c5caba4a084
JoelCruzCode <136981207+JoelCruzCode@users.noreply.github.com>
2023-07-16 09:06:41 -0700

    Split event listeners

--- script.js
+++ script.js
@@ -5,18 +5,16 @@ let firstNum;
 let secondNum;
 let operation;
 
-const buttons = document.querySelectorAll("button");
-const subBtn = document.querySelector(".substract");
+const numbers = document.querySelectorAll(".number");
+const subBtn = document.querySelector(".subtract");
 const addBtn = document.querySelector(".add");
 const multBtn = document.querySelector(".multiply");
 const divBtn = document.querySelector(".divide");
+const arithmetics = [subBtn, addBtn, multBtn, divBtn];
+//
 const equalBtn = document.querySelector(".equal");
 const clearBtn = document.querySelector(".clear");
-const squareBtn = document.querySelector(".square-root");
-const expBtn = document.querySelector(".exponent");
-const caclculator = document.querySelector(".calculator");
 const display = document.querySelector(".display");
-const Arithmetics = [subBtn, addBtn, multBtn, divBtn];
 
 // Math Operations
 const add = (a, b) => a + b;
@@ -42,23 +40,35 @@ const operate = function (operator, num1, num2) {
 const displayContent = function (content) {
   display.textContent += ` ${content}`;
 };
+
+const clear = function () {
+  display.textContent = ``;
+  firstNum = "";
+  secondNum = "";
+  operation = "";
+};
+
 // Event Listeners
-buttons.forEach((btn) =>
-  btn.addEventListener("click", function (e) {
-    if (e.target.className != "number") {
-      operation = e.target.textContent;
-      displayContent(operation);
-    } else if ((e.target.className = "number")) {
-      if (!firstNum) {
-        firstNum = e.target.textContent;
-        displayContent(firstNum);
-      } else {
-        secondNum = e.target.textContent;
-        displayContent(secondNum);
-      }
+arithmetics.forEach((btn) =>
+  btn.addEventListener("click", (e) => {
+    operation = e.target.textContent;
+    displayContent(operation);
+  })
+);
+
+numbers.forEach((btn) =>
+  btn.addEventListener("click", (e) => {
+    if (!firstNum) {
+      firstNum = e.target.textContent;
+      displayContent(firstNum);
+    } else {
+      secondNum = e.target.textContent;
+      displayContent(secondNum);
     }
   })
 );
+
+clearBtn.addEventListener("click", clear);
 
 //////////////////////////////////////////////////////////
 
@@ -79,3 +89,30 @@ buttons.forEach((btn) =>
 //   const root = (a, b) => {};
 
 // createNumbers();
+
+/////////////////////
+
+// const buttons = document.querySelectorAll("button");
+// const squareBtn = document.querySelector(".square-root");
+// const expBtn = document.querySelector(".exponent");
+//
+// const displayContent = function (content) {
+//     display.textContent += ` ${content}`;
+//   };
+//   // Event Listeners
+//   buttons.forEach((btn) =>
+//     btn.addEventListener("click", function (e) {
+//       if (e.target.className != "number") {
+//         operation = e.target.textContent;
+//         displayContent(operation);
+//       } else if ((e.target.className = "number")) {
+//         if (!firstNum) {
+//           firstNum = e.target.textContent;
+//           displayContent(firstNum);
+//         } else {
+//           secondNum = e.target.textContent;
+//           displayContent(secondNum);
+//         }
+//       }
+//     })
+//   );
