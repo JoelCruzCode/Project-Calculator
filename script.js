1db35a2967131a5ded0cf7ba97f8c3cc63a3c728
JoelCruzCode <136981207+JoelCruzCode@users.noreply.github.com>
2023-07-16 08:35:18 -0700

    Create event listener to display content

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+// To do List:
+// 1.) seperate equal button from event listener
+
+let firstNum;
+let secondNum;
+let operation;
+
+const buttons = document.querySelectorAll("button");
 const subBtn = document.querySelector(".substract");
 const addBtn = document.querySelector(".add");
 const multBtn = document.querySelector(".multiply");
@@ -7,6 +15,10 @@ const clearBtn = document.querySelector(".clear");
 const squareBtn = document.querySelector(".square-root");
 const expBtn = document.querySelector(".exponent");
 const caclculator = document.querySelector(".calculator");
+const display = document.querySelector(".display");
+const Arithmetics = [subBtn, addBtn, multBtn, divBtn];
+
+// Math Operations
 const add = (a, b) => a + b;
 
 const subtract = (a, b) => a - b;
@@ -15,20 +27,55 @@ const multiply = (a, b) => a * b;
 
 const divide = (a, b) => a / b;
 
-const exponent = (a, b) => {
-  let sum = 1;
-  for (i = 1; i <= b; i++) sum *= a;
-  return sum;
-};
-
-const root = (a, b) => {};
-
-const createNumbers = function () {
-  for (i = 0; i < 10; i++) {
-    let num = document.createElement("button");
-    num.textContent = i;
-    caclculator.append(num);
+const operate = function (operator, num1, num2) {
+  if (operator === "-") {
+    return subtract(num1, num2);
+  } else if (operator === "+") {
+    return add(num1, num2);
+  } else if (operator === "*") {
+    return multiply(num1, num2);
+  } else if (operator === "/") {
+    return divide(num1, num2);
   }
 };
 
-createNumbers();
+const displayContent = function (content) {
+  display.textContent += ` ${content}`;
+};
+// Event Listeners
+buttons.forEach((btn) =>
+  btn.addEventListener("click", function (e) {
+    if (e.target.className != "number") {
+      operation = e.target.textContent;
+      displayContent(operation);
+    } else if ((e.target.className = "number")) {
+      if (!firstNum) {
+        firstNum = e.target.textContent;
+        displayContent(firstNum);
+      } else {
+        secondNum = e.target.textContent;
+        displayContent(secondNum);
+      }
+    }
+  })
+);
+
+//////////////////////////////////////////////////////////
+
+// const createNumbers = function () {
+//   for (i = 0; i < 10; i++) {
+//     let num = document.createElement("button");
+//     num.textContent = i;
+//     caclculator.append(num);
+//   }
+// };
+
+// const exponent = (a, b) => {
+//     let sum = 1;
+//     for (i = 1; i <= b; i++) sum *= a;
+//     return sum;
+//   };
+
+//   const root = (a, b) => {};
+
+// createNumbers();
